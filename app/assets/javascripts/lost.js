bb2e7a56cacf3c230457d03880781a8a24d85ad0
Will Djingga <wdjingga@gmail.com>
2015-07-03 21:44:22 +1000

    Geolocation first attempt

--- app/assets/javascripts/lost.js
+++ app/assets/javascripts/lost.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function getlocation() {
+	navigator.geolocation.getCurrentPosition(
+		function(p) {
+			alert(p.coords.latitude);
+
+		},
+		function () {
+			alert('Error locating your location :(');
+		}
+	)}
