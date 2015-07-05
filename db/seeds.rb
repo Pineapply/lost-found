a920e00687c8fda1cfd0ccbbc360af3f9ad61791
Will Djingga <wdjingga@gmail.com>
2015-07-05 12:35:12 +1000

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Missing.create(name: "Red book left on 109 tram", description: "Book with red stickers on the cover. It's not very expensive but has sentimental value to me personally :( Please return if found", type:"Missing", thumbnail: "https://raw.githubusercontent.com/Boo-Hiss/lost-found/master/app/assets/images/red-book.jpeg" )
 Missing.create(name: "Umbrella", description: "Hi... I left my black umbrella. Has anyone seen it???", type:"Missing", thumbnail: "https://raw.githubusercontent.com/Boo-Hiss/lost-found/master/app/assets/images/black-umbrella.jpeg")
-Missing.create(name: "Floral fabric wallet", description: "Floral fabric wallet left on tram (or tram stop?).", type:"Missing",thumbnail: "https://raw.githubusercontent.com/Boo-Hiss/lost-found/master/app/assets/images/floral-wallet.jpeg")
+Missing.create(name: "Floral fabric wallet", description: "Floral fabric wallet left on tram (or tram stop?).", type:"Missing",thumbnail: "https://raw.githubusercontent.com/Boo-Hiss/lost-found/master/app/assets/images/floral-wallet-red.jpeg")
 Missing.create(name: "iPhone in pink case", description: "Have been reported to Telstra as missing so IMEI is blocked. Please return as I needed the photos. Thank you in advance.", type:"Missing",thumbnail: "https://raw.githubusercontent.com/Boo-Hiss/lost-found/master/app/assets/images/pink-iphone.jpeg")
 Missing.create(name: "Laptop", description: "Macbook-pro 15 inches with Github sticker on the cover", type:"Missing",thumbnail: "https://raw.githubusercontent.com/Boo-Hiss/lost-found/master/app/assets/images/laptop.jpeg")
 
